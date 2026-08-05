61a33c32d6182503a4f7f5f865d0b0799ce628bc
thelevnet <dev@lev-net.xyz>
2026-08-05 12:07:03 +0200

    clean

--- apps.nix
+++ apps.nix
@@ -1,120 +1,39 @@
-{ config, pkgs, quickshellWrapped,...}:
+{ config, pkgs, inputs, ...}:
 {
 environment.systemPackages = with pkgs; [
     #next start
-		python3
-		kotlin
-		swift
-		viber
+		qrencode
 		fzf
 		btop
-    #next end
     portablemc
 	  nh
     fd
     vlc
-    libsForQt5.qtstyleplugin-kvantum
     rustup
     discord
     gcc
-    vscode
     lua-language-server
     stylua
     unzip
-    micro
     wget
     git
-    python3
     bat
-    gh
     nasm
     fastfetch
     eza
-    lxsession
-    gsettings-desktop-schemas
-    glib
-    quickshellWrapped
-    upower
-    papirus-icon-theme
-    adwaita-icon-theme
-    kdePackages.breeze-icons
-    gnome-icon-theme
-    hyprsunset
-    hypridle
     kdePackages.filelight
-    hyprpicker
-    hyprshot
-    wl-clipboard
-    playerctl
-    libnotify
     kitty
-    fuzzel
-    matugen
     bibata-cursors
-    adw-gtk3
-    darkly
-    material-symbols
     nerd-fonts.jetbrains-mono
-    rubik
-    twemoji-color-font
     kdePackages.dolphin
-    libcava
-    lxqt.pavucontrol-qt
-    wireplumber
-    libdbusmenu-gtk3
-    songrec
-    slurp
-    swappy
-    wf-recorder
-    tesseract
-    imagemagick
-    bc
-    cliphist
     cmake
-    curlFull
     ripgrep
-    jq
-    rsync
-    yq-go
-    xdg-user-dirs
-    upower
-    wtype
-    ydotool
-    translate-shell
-    libqalculate
-    uv
-    gtk4
-    libadwaita
-    libsoup_3
-    libportal-gtk4
-    gobject-introspection
-    fontconfig
+    inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
     brightnessctl
-    ddcutil
     google-chrome
     telegram-desktop
-    grim
-    cowsay
-    cmatrix
-    sl
-    aseprite
     neovim
-    (geoclue2.override { withDemoAgent = true; })
-    (pkgs.writeShellApplication {
-      name = "lns";
-      runtimeInputs = [ pkgs.nasm pkgs.binutils ]; 
-      text = ''
-        if [ -z "$1" ]; then
-          echo "Error: No filename provided."
-          echo "Usage: build-asm <filename_without_extension>"
-          exit 1
-          fi
-        
-          nasm -f elf64 "$1.asm" -o temp.o
-          ld -o "$1" temp.o
-          rm temp.o
-      '';
-    })
-];
+    #next end
+  ];
 }
 
--- flake.nix
+++ flake.nix
@@ -2,27 +2,18 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     hyprland.url = "github:hyprwm/Hyprland";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    quickshell = {
-      url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
+    noctalia = {
+      url = "github:noctalia-dev/noctalia";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     playit-nixos-module.url = "github:pedorich-n/playit-nixos-module";
   };
-  outputs = { self, nixpkgs, hyprland, home-manager, quickshell, playit-nixos-module, ... } @ inputs: {
-    nixosConfigurations."neto" = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, hyprland, playit-nixos-module, ... } @ inputs: {
+    nixosConfigurations."nix" = nixpkgs.lib.nixosSystem {
       specialArgs = { inherit inputs; };
       modules = [
-        # Use inputs.playit-nixos-module to access the module
         inputs.playit-nixos-module.nixosModules.default
         ./configuration.nix
-        home-manager.nixosModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-        }
       ];
     };
   };}
