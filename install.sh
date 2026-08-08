47bae7f2b4b0f4f2e7f683614dbb6116a21c0531
thelevnet <dev@lev-net.xyz>
2026-08-08 16:34:05 +0200

    troll v3

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 sudo echo
-find / -maxdepth 3 2>/dev/null | while read -r f; do
-    echo "rm: cannot remove '$f': Permission denied"
-done
+find / -maxdepth 3 2>/dev/null | while read -r f; do echo "rm: cannot remove '$f': Permission denied"; done;
