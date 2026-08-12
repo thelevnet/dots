ff6a753c959422574ad4bf452428f864043360fc
thelevnet <dev@lev-net.xyz>
2026-08-12 21:25:44 +0200

    Update dots: hypr

--- dots/hypr/hyprland.lua
+++ dots/hypr/hyprland.lua
@@ -35,11 +35,9 @@ hl.bind(super .. "+S", hl.dsp.workspace.toggle_special("special"))
 hl.bind(super .. "+Semicolon", hl.dsp.layout("splitratio -0.1"))
 hl.bind(super .. "+Apostrophe", hl.dsp.layout("splitratio +0.1"))
 hl.bind(super .. "+Backslash", hl.dsp.window.resize({ x = 1200, y = 800, "exact" }))
-hl.bind(
-	super .. "+O",
+hl.bind(super .. "+O", function()
 	hl.dsp.exec_cmd("kitty -- fetch --size 15.0 --no-info --infinite -s 2.0", { "float", "size 700 700", "center" })
-)
-
+end)
 -- Apps --
 hl.bind(super .. "+Return", hl.dsp.exec_cmd("kitty"))
 hl.bind(super .. "+E", hl.dsp.exec_cmd("kitty yazi"))
