21d21cd8ee2185abc55a6b62baf4f2ef09ef2dea
thelevnet <dev@lev-net.xyz>
2026-09-04 21:27:34 +0200

    Update dots: hypr

--- dots/hypr/hyprland.lua
+++ dots/hypr/hyprland.lua
@@ -284,7 +284,7 @@ hl.config({
 	misc = {
 		disable_hyprland_logo = true,
 		disable_splash_rendering = true,
-		vrr = 0,
+		vrr = 1,
 		mouse_move_enables_dpms = true,
 		key_press_enables_dpms = true,
 		animate_manual_resizes = false,
